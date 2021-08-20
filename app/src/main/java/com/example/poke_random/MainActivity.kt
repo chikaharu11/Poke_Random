ef2bb13b0620dc4fd8eaf33803b0f5c6b5d2033e
chikaharu11 <giza55555@hotmail.com>
2021-08-20 13:29:33 +0900

    add random

--- app/src/main/java/com/example/poke_random/MainActivity.kt
+++ app/src/main/java/com/example/poke_random/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.poke_random
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        val button = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.setInitialScale(1)
+        webView.loadUrl("file:///android_asset/card_back.html")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?,
+            ): Boolean {
+                return false
+            }
+        }
+
+        button.setOnClickListener{
+            val random = (1..893).random()
+            webView.loadUrl("https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:$random")
+        }
     }
 }
